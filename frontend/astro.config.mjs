b8df326ccfdb74a3cc0ccaba46c398d49a8fb91e
yoezequiel <ezequielcasiano15@gmail.com>
2025-12-23 18:08:44 -0300

    chore: update package.json to include @astrojs/vercel and add vercel.json configuration

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
     integrations: [tailwind()],
     output: "server",
-    adapter: node({
-        mode: "standalone",
-    }),
+    adapter: vercel(),
     server: {
         port: 4321,
         host: true,
