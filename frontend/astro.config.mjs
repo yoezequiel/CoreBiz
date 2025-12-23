a6d55411df79acaa7291d31e3a635cfa4906a22b
yoezequiel <ezequielcasiano15@gmail.com>
2025-12-23 18:46:39 -0300

    feat: agregar configuración del sitio en astro.config.mjs

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
+    site: "https://corebiz.vercel.app",
     integrations: [tailwind()],
     output: "server",
     adapter: vercel(),
